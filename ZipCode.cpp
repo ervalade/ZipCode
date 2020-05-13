a36d1a1f882ca93ec8adc616017cd6f292ff23ba
ervalade <ervalade@gmail.com>
2020-05-13 14:39:16 +0200

    some changes on unit tests

--- ZipCode.cpp
+++ ZipCode.cpp
@@ -6,30 +6,38 @@
  */
 
 #include <ZipCode.h>
-const std::map<int, Digit> ZipCode::convert = { { 1, Digit::one }, { 2,
-		Digit::two }, { 3, Digit::three }, { 4, Digit::four },
-		{ 5, Digit::five }, { 6, Digit::six }, { 7, Digit::seven }, { 8,
-				Digit::eight }, { 9, Digit::nine }, { 0, Digit::zero } };
+const std::map<int, Digit> ZipCode::convert =
+  {
+    { 1, Digit::one },
+    { 2, Digit::two },
+    { 3, Digit::three },
+    { 4, Digit::four },
+    { 5, Digit::five },
+    { 6, Digit::six },
+    { 7, Digit::seven },
+    { 8, Digit::eight },
+    { 9, Digit::nine },
+    { 0, Digit::zero } };
 //const int ZipCode::LENGTH = 5;
 
 ZipCode::ZipCode(const unsigned int zipValue) { //liste d'initialisation
-	//convert int to std::array<Digit,5>
-	//1000
-	if (zipValue < 1 || zipValue > 99999)
-		throw std::string("zip code MUST be between 1 and 99999");
-	int value = zipValue;
-	for (int i = 0, unit = 10000; i < ZipCode::LENGTH; i++, unit /= 10) {
-		this->value.at(i) = ZipCode::convert.at(value / unit);
-		value %= unit;
-	}
+  //convert int to std::array<Digit,5>
+  //1000
+  if (zipValue < 1 || zipValue > 99999)
+    throw std::string("zip code MUST be between 1 and 99999");
+  int value = zipValue;
+  for (int i = 0, unit = 10000; i < ZipCode::LENGTH; i++, unit /= 10) {
+    this->value.at(i) = ZipCode::convert.at(value / unit);
+    value %= unit;
+  }
 }
 
 ZipCode::~ZipCode() {
-	// TODO Auto-generated destructor stub
+  // TODO Auto-generated destructor stub
 }
 
 const std::array<Digit, ZipCode::LENGTH>& ZipCode::getValue() const {
-	return value;
+  return value;
 }
 /* operator << as member : friend not needed
  std::ostream & ZipCode::operator<<(std::ostream & os) {
@@ -40,16 +48,16 @@ const std::array<Digit, ZipCode::LENGTH>& ZipCode::getValue() const {
  }
  */
 std::ostream& operator<<(std::ostream &os, const ZipCode &z) {
-	//z.operator <<(os);// operator << as member  : friend not needed
-	for (auto &digit : z.getValue()) {
-		os << (int) digit;
-	}
-	return os;
+  //z.operator <<(os);// operator << as member  : friend not needed
+  for (auto &digit : z.getValue()) {
+    os << (int) digit;
+  }
+  return os;
 }
-
+//#define _ZIPCODE_UT_ //only to enable source, format, don't forget to comment in
 #ifdef _ZIPCODE_UT_
 /*
-g++ -D _ZIPCODE_UT_ -o ZipCodeUt ZipCode.cpp -std=c++14 -I ./ && ./ZipCodeUt
+ g++ -D _ZIPCODE_UT_ -o ZipCodeUt ZipCode.cpp -std=c++14 -I ./ && ./ZipCodeUt
  */
 #include <iostream>
 #include <sstream>
@@ -58,32 +66,79 @@ g++ -D _ZIPCODE_UT_ -o ZipCodeUt ZipCode.cpp -std=c++14 -I ./ && ./ZipCodeUt
 #include <cassert>
 
 int main(int argc, char **argv) {
-	std::map<int,std::string> testCases={
-			{1,"00001"},
-			{12,"00012"},
-			{123,"00123"},
-			{1234,"01234"},
-			{12345,"12345"},
-			{6789,"06789"},
-			{99999,"99999"},
-			{0,"zip code MUST be between 1 and 99999"},
-			{100000,"zip code MUST be between 1 and 99999"}
+  {
+    std::cout << "ZipCode constructor Tests" << std::endl;
+    std::map<int, std::array<Digit, ZipCode::LENGTH> > testCases =
+      { //
+              { 1,
+                    { Digit::zero, Digit::zero, Digit::zero, Digit::zero,
+                        Digit::one } }, //
+              { 12,
+                    { Digit::zero, Digit::zero, Digit::zero, Digit::one,
+                        Digit::two } },  //
+              { 123,
+                    { Digit::zero, Digit::zero, Digit::one, Digit::two,
+                        Digit::three } }, //
+              { 1234,
+                    { Digit::zero, Digit::one, Digit::two, Digit::three,
+                        Digit::four } },  //
+              { 12345,
+                    { Digit::one, Digit::two, Digit::three, Digit::four,
+                        Digit::five } }, //
+              { 6789,
+                { Digit::zero, Digit::six, Digit::seven, Digit::eight,
+                    Digit::nine } }, //
+              { 99999,
+                { Digit::nine, Digit::nine, Digit::nine, Digit::nine,
+                    Digit::nine } }, //
+              { 0,
+                { Digit::zero, Digit::zero, Digit::zero, Digit::zero,
+                    Digit::zero } }, // exception
+              { 100000,
+                { Digit::zero, Digit::zero, Digit::zero, Digit::zero,
+                    Digit::zero } } //exception
+        };
+    for (auto &testCase : testCases) {
+      try {
+        std::cout << "Test value : " << testCase.first << std::endl;
+        ZipCode zipCode(testCase.first);
+        assert(zipCode.getValue() == testCase.second);
+      } catch (const std::string &e) {
+        assert(e == "zip code MUST be between 1 and 99999");
+      }
+    }
+  }
 
-	};
-	for(auto const testCase:testCases){
-		try{
-		ZipCode zipCode(testCase.first);
-	//zipCode.operator<<(std::cout);// operator << as member  : friend not needed
-	//	std::cout << zipCode << std::endl;
-		std::ostringstream oss;
+  {
+    std::cout << "ZipCode operator << Tests" << std::endl;
+    std::map<int, std::string> testCases =
+      { //
+          { 1, "00001" }, //
+              { 12, "00012" }, //
+              { 123, "00123" }, //
+              { 1234, "01234" }, //
+              { 12345, "12345" }, //
+              { 6789, "06789" }, //
+              { 99999, "99999" }, //
+              { 0, "zip code MUST be between 1 and 99999" }, //
+              { 100000, "zip code MUST be between 1 and 99999" } //
 
-		oss <<zipCode ;
-	assert(oss.str()==testCase.second);
-		}catch(const std::string& e){
-			assert(e==testCase.second);
-		}
-	}
-	return 0;
+        };
+    for (auto &testCase : testCases) {
+      try {
+        std::cout << "Test value : " << testCase.first << std::endl;
+        ZipCode zipCode(testCase.first);
+        //zipCode.operator<<(std::cout);// operator << as member  : friend not needed
+        //	std::cout << zipCode << std::endl;
+        std::ostringstream oss;
+        oss << zipCode;
+        assert(oss.str() == testCase.second);
+      } catch (const std::string &e) {
+        assert(e == testCase.second);
+      }
+    }
+  }
+  return 0;
 }
 
 #endif
--- ZipCode.h
+++ ZipCode.h
@@ -20,34 +20,34 @@
  * @enduml
  */
 enum class Digit {
-	one = 1,
-	two = 2,
-	three = 3,
-	four = 4,
-	five = 5,
-	six = 6,
-	seven = 7,
-	eight = 8,
-	nine = 9,
-	zero = 0
+  one = 1,
+  two = 2,
+  three = 3,
+  four = 4,
+  five = 5,
+  six = 6,
+  seven = 7,
+  eight = 8,
+  nine = 9,
+  zero = 0
 };
 #include <array>
 #include <iostream>
 
 class ZipCode {
-public:
-	static const int LENGTH = 5; //static-> défini une seule fois appelée variable (attribut) de classe
-	//std::ostream & operator<<(std::ostream & os);operator << as member : friend not needed
-	friend std::ostream& operator<<(std::ostream &os, const ZipCode &z);
+  public:
+    static const int LENGTH = 5; //static-> défini une seule fois appelée variable (attribut) de classe
+    //std::ostream & operator<<(std::ostream & os);operator << as member : friend not needed
+    friend std::ostream& operator<<(std::ostream &os, const ZipCode &z);
 
-private:
-	std::array<Digit, ZipCode::LENGTH> value;//défini pour chaque objet (instance de classe) appelée variable d'instance
-	static const std::map<int, Digit> convert;//static-> défini une seule fois appelée variable (attribut) de classe
+  private:
+    std::array<Digit, ZipCode::LENGTH> value;	//défini pour chaque objet (instance de classe) appelée variable d'instance
+    static const std::map<int, Digit> convert;//static-> défini une seule fois appelée variable (attribut) de classe
 
-public:
-	ZipCode(const unsigned int zipValue = 0);//construteur : appeler automatiquement à l'instanciation
-	const std::array<Digit, ZipCode::LENGTH>& getValue() const;
-	virtual ~ZipCode();	//destructeur appeler automatiquement à la suppression
+  public:
+    ZipCode(const unsigned int zipValue = 0);	//construteur : appeler automatiquement à l'instanciation
+    const std::array<Digit, ZipCode::LENGTH>& getValue() const;
+    virtual ~ZipCode();	//destructeur appeler automatiquement à la suppression
 };
 
 #endif /* ZIPCODE_H_ */
